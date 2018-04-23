f34086b53578e70a8025bd73198323430723a797
Matthieu Coudron <mattator@gmail.com>
2018-04-23 19:08:55 +0900

    WIP

--- mptcp-local-logical.nix
+++ mptcp-local-logical.nix
@@ -3,6 +3,69 @@
 { multihomed ? false , ... }:
 let
   tpl = { config, pkgs, lib,  ... }:
+  let
+    ip = "${pkgs.iproute}/bin/ip";
+    logger = "${pkgs.utillinux}/bin/logger";
+    
+mptcpUp =   /home/teto/dotfiles/nixpkgs/hooks/mptcp_up_raw;
+# builtins.readFile
+    #mptcpUp = writeScript ''
+##!/bin/sh
+#set -ex
+
+#env 
+## > /tmp/if_up_env
+
+#if [ "$IFACE" = lo ] || [ "$MODE" != start ]; then
+
+	#logger "if localhost or $MODE then abort "
+	#exit 0
+#fi
+
+#if [ -z "$DEVICE_IFACE" ]; then
+
+	#${utillinux}/logger "invalid \$DEVICE_IFACE"
+	#exit 0
+#fi
+
+## mkdir -p /etc/iproute2
+## FIRST, make a table-alias
+#if [ `grep "$DEVICE_IFACE" /etc/iproute2/rt_tables | wc -l` -eq 0 ]; then
+	#${utillinux}/logger "Adding to iproute2/rt_tables \$DEVICE_IFACE"
+	#NUM=$(wc -l < /etc/iproute2/rt_tables)
+	#echo "$NUM  $DEVICE_IFACE" >> /etc/iproute2/rt_tables
+
+#fi
+
+#if [ "$DHCP4_IP_ADDRESS" ]; then
+	#SUBNET=`echo $IP4_ADDRESS_0 | cut -d \   -f 1 | cut -d / -f 2`
+	#${ip} route add table "$DEVICE_IFACE" to "$DHCP4_NETWORK_NUMBER/$SUBNET" dev "$DEVICE_IFACE" scope link
+	#${ip} route add table "$DEVICE_IFACE" default via $DHCP4_ROUTERS dev "$DEVICE_IFACE"
+	#${ip} rule add from $DHCP4_IP_ADDRESS table "$DEVICE_IFACE"
+#else
+	## PPP-interface
+	#IPADDR=`echo $IP4_ADDRESS_0 | cut -d \   -f 1 | cut -d / -f 1`
+	#${ip} route add table $DEVICE_IFACE default dev $DEVICE_IP_IFACE scope link
+	#${ip} rule add from $IPADDR table $DEVICE_IFACE
+#fi
+#'';
+
+mptcpDown =  /home/teto/dotfiles/nixpkgs/hooks/mptcp_down_raw;
+    # mptcpDown = pkgs.writeScript "mptcp_down" ''
+##!/bin/sh
+#set -ex
+
+## env > /tmp/if_down_env
+
+#if [ "$IFACE" = lo -o "$MODE" != stop ]; then
+#        exit 0
+#fi
+
+#${ip} rule del table $DEVICE_IFACE
+#${ip} route flush table $DEVICE_IFACE 
+#''
+
+    in
   ({
     # prints everything superior to this number
 
@@ -40,19 +103,19 @@ let
     # TODO reestablish with the correct nixpkgs !
     dispatcherScripts = [
       {
-        source = /home/teto/dotfiles/nixpkgs/modules/mptcp_up;
+        source = mptcpUp;
         type = "up";
       }
       {
-        source = /home/teto/dotfiles/nixpkgs/modules/mptcp_down ;
+        source = mptcpDown;
         type = "down";
       }
-      ];
+    ];
 
     # networking.resolvconfOptions
     # wifi.powersave=false;
     # TODO configure dispatcherScripts  for mptcp
-  }
+  };
 
     # nixpkgs.overlays = [ (import ./overlays/this.nix) (import ./overlays/that.nix) ]
     # nixpkgs.overlays = [
