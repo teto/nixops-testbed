770a05699275cef6c1eddba377c6eb9cc0dd850c
Matthieu Coudron <mattator@gmail.com>
2018-08-07 18:47:11 +0900

    adapted script

--- attach_tc.sh
+++ attach_tc.sh
@@ -1,5 +1,6 @@
-sudo tc qdisc add dev eth0 clsact
+IF="enp4s0"
+sudo tc qdisc add dev "$IF" clsact
 
-sudo tc filter add dev  eth0 ingress bpf obj test_ebpf_tc.o section action direct-action
+sudo tc filter add dev "$IF" ingress bpf obj test_ebpf_tc.o section action direct-action
 
 # sudo cat /sys/kernel/debug/tracing/trace_pipe
