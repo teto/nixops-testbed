abdd5122cec5a6b052400c7ac6c27455c0c02060
Matthieu Coudron <mattator@gmail.com>
2018-08-08 16:33:41 +0900

    ok

--- attach_tc.sh
+++ attach_tc.sh
@@ -1,6 +1,9 @@
 IF="enp4s0"
 sudo tc qdisc add dev "$IF" clsact
 
-sudo tc filter add dev "$IF" ingress bpf obj test_ebpf_tc.o section action direct-action
+# le "Note: 8 bytes struct bpf_elf_map fixup performed due to size mismatch! est normal
+sudo tc filter add dev "$IF" ingress bpf obj test_ebpf_tc.o section action verbose direct-action
+
+# to remove the filter, one can then do
 
 # sudo cat /sys/kernel/debug/tracing/trace_pipe
